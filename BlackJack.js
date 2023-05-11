603355184f234ac24c436411d4765a9fca69d32b
AngeloBla <angeloblajic@gmail.com>
2023-05-11 15:18:03 +0200

    Update BlackJack.js
    .

--- BlackJack.js
+++ BlackJack.js
@@ -21,42 +21,49 @@ function closePopup() {
 
 let summeSpieler = 0;
 
+function berechne_summe_karten(karten) {
+  let summe = 0;
+  for (let i = 0; i < karten.length; i++) {
+    summe += karten[i];
+  }
+  return summe;
+}
+
 
 function spiel_starten() {
-    for (let i = 0; i < 2; i++) {
-      const randomIndex = Math.floor(Math.random() * deck.length);
-      deck.splice(randomIndex, 1);
-      spieler.push(deck[randomIndex]);
-      spielfeld_spieler.innerHTML += '<img src="cards/' + deck_img[randomIndex] + '">';
-    }
+  for (let i = 0; i < 2; i++) {
     const randomIndex = Math.floor(Math.random() * deck.length);
     deck.splice(randomIndex, 1);
-    bank.push(deck[randomIndex]);
-    spielfeld_pc.innerHTML += '<img src="cards/' + deck_img[randomIndex] + '">';  //Möglicher Konflikt mit +?
-}
-  
+    spieler.push(deck[randomIndex]);
+    spielfeld_spieler.innerHTML += '<img src="cards/' + deck_img[randomIndex] + '">';
+  }
+  const randomIndex = Math.floor(Math.random() * deck.length);
+  deck.splice(randomIndex, 1);
+  bank.push(deck[randomIndex]);
+  spielfeld_pc.innerHTML += '<img src="cards/' + deck_img[randomIndex] + '">';  //Möglicher Konflikt mit +?
+  summeSpieler = berechne_summe_karten(spieler);
+  Spielstand();
+}  
 
 function karte_ziehen() {
   const randomIndex = Math.floor(Math.random() * deck.length);
   spieler.push(deck[randomIndex]);
   deck.splice(randomIndex, 1);
   spielfeld_spieler.innerHTML += '<img src="cards/' + deck_img[randomIndex] + '">';
+  summeSpieler = berechne_summe_karten(spieler);
+  Spielstand();
 }
 
 
 function Spielstand() {
-    summeSpieler = 0;
-    for (let i = 0; i < spieler.length; i++) {
-      summeSpieler += spieler[i];
-    }
-    if (summeSpieler > 21) {
-      alert("Sie haben verloren. Ihre Karten haben einen Wert von " + summeSpieler);
-    } else if (summeSpieler === 21) {
-      alert("Sie haben GEWONNEN! Ihre Karten haben einen Wert von " + summeSpieler);
-    } else if (summeSpieler < 21) {
-      alert(summeSpieler + " Möchten Sie eine weitere Karte ziehen?");
-    }
-    document.getElementById("stand").innerHTML = summeSpieler;
+  if (summeSpieler > 21) {
+    alert("Sie haben verloren. Ihre Karten haben einen Wert von " + summeSpieler);
+  } else if (summeSpieler === 21) {
+    alert("Sie haben GEWONNEN! Ihre Karten haben einen Wert von " + summeSpieler);
+  } else if (summeSpieler < 21) {
+    alert(summeSpieler + " Möchten Sie eine weitere Karte ziehen?");
+  }
+  document.getElementById("stand").innerHTML = summeSpieler;
 }
 
 
@@ -66,16 +73,18 @@ function passen() {
 
 
 function pruefe_bank() {
-  let summeBank = 0;
-  for (let i = 0; i < bank.length; i++) {
-    summeBank += bank[i];
+  let summeBank = berechne_summe_karten(bank);
+  if(summeBank > 21){
+    alert("Sie haben gewonnen!")
   }
-  if (summeBank === 21) {
+  else if (summeBank === 21) {
     gewinner_ermitteln(summeBank, summeSpieler);
-  } else if (summeBank < 17) {
+  } 
+  else if (summeBank < 17) {
     karte_ziehen_Bank();
     pruefe_bank();
-  } else if (summeBank > 17) {
+  } 
+  else if (summeBank > 17) {
     gewinner_ermitteln(summeBank, summeSpieler);
   }
 }
@@ -85,6 +94,7 @@ function karte_ziehen_Bank() {
   bank.push(deck[randomIndex]);
   deck.splice(randomIndex, 1);
   spielfeld_pc.innerHTML += '<img src="cards/' + deck_img[randomIndex] + '">';
+  pruefe_bank();
 }
 
 
@@ -96,4 +106,14 @@ function gewinner_ermitteln(summeBank, summeSpieler) {
   } else if (summeBank === summeSpieler) {
     alert(summeSpieler + " Unentschieden");
   }
+}
+
+function neu_beginnen() {
+  spieler.length = 0; 
+  bank.length = 0; 
+  deck.length = 13; 
+  spielfeld_spieler.innerHTML = ''; 
+  spielfeld_pc.innerHTML = ''; 
+  summeSpieler = 0; 
+  document.getElementById("stand").innerHTML = summeSpieler;
 }
